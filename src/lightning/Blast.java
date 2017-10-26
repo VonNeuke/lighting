2020aada7f953dd662818b4c4b86a41308b788c6
VonNeuke <1340171924@qq.com>
2017-10-26 15:18:37 +0800

    first commit

---
+++ src/lightning/Blast.java
@@ -0,0 +1,22 @@
+package lightning;
+//package lightning;
+
+class Blast {
+  int x;
+  int y;
+  int cont;
+  boolean islive=true;
+  Blast(int x,int y) {
+    this.x=x;
+    this.y=y;
+    this.cont=0;
+  }
+  public void grow(){
+    if(cont>21){
+      this.islive=false;
+    }
+    this.cont++;
+    this.y++;
+  }
+
+}
